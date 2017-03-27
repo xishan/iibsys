1a7e0a4e3e313825c4124e8ff391020aaa0fe3d8
Zeeshan Abbas <zeeshan@iibsys.com>
2017-03-27 14:05:45 +0300

    The Application Structure.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * index.php
+ *
+ * Redirects to the public system.
+ *
+ * @copyright 
+ * @license 
+ * @version 
+ * @package 
+ */
+
+// Do the initial testing 
+
+// If we are, go ahead to the public domain
+header('Location: public/');
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * index.php
+ *
+ * Redirects to the public system.
+ *
+ * @copyright 
+ * @license 
+ * @version 
+ * @package 
+ */
+
+// The Application's Public Domain.
+
+
